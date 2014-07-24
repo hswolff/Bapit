b9a7281407b0e431ece367225fe21a87e428c671
Harry Wolff <hswolff@gmail.com>
2014-07-23 22:09:59 -0400

    Create initial functionality of game.
    Adds a ball to the scene initially.
    Adds a physics body to the ball.
    
    Allows you to tap anywhere in the screen and an impulse is applied
    to the ball which causes it to lift from the bottom of the scene.

--- Bapit/GameScene.swift
+++ Bapit/GameScene.swift
@@ -9,37 +9,34 @@
 import SpriteKit
 
 class GameScene: SKScene {
-    override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
-        self.addChild(myLabel)
-    }
+  let ballRadius: CGFloat
+  let ball: SKShapeNode
+
+  init(coder aDecoder: NSCoder!)  {
+    self.ballRadius = 75
+    self.ball = SKShapeNode(circleOfRadius: self.ballRadius)
+    super.init(coder: aDecoder)
+  }
+
+  override func didMoveToView(view: SKView) {
+    self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+
+    ball.fillColor = UIColor.blackColor()
+    ball.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+    ball.physicsBody = SKPhysicsBody(circleOfRadius: ballRadius)
+    ball.physicsBody.allowsRotation = false
+    self.addChild(ball)
+  }
     
-    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-        /* Called when a touch begins */
+  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+    /* Called when a touch begins */
         
-        for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+    for touch: AnyObject in touches {
+      ball.physicsBody.applyImpulse(CGVectorMake(0, 500))
     }
+  }
    
-    override func update(currentTime: CFTimeInterval) {
-        /* Called before each frame is rendered */
-    }
+  override func update(currentTime: CFTimeInterval) {
+    /* Called before each frame is rendered */
+  }
 }
--- Bapit/GameViewController.swift
+++ Bapit/GameViewController.swift
@@ -45,23 +45,6 @@ class GameViewController: UIViewController {
         }
     }
 
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
-        } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
