ff97381bece2727104ef53d2b31179832e091a53
Harry Wolff <hswolff@gmail.com>
2014-08-08 23:50:12 -0400

    This creates my first enum and struct.
    The enum is for PhysicsBody category and collision bit mask, to allow
    me to handle contacts between two physics bodies correctly.
    
    The struct is to create a data type for storing the amount of taps
    that someone correctly tapped the ball, and someone missed it in case
    I'd want to use that information.
    
    Also moved to instantiating each property in the initializer function init().
    
    Also sped up the game to make it be a little more challenging.
    
    Created the bottomBorder to know when the ball drops to the bottom.
    
    Created scoreLabel to show the current score of the game.
    
    Used the willSet observer to get notified when the TapCount struct is
    modified so we can then update the scoreLabel correctly.
    
    Add an extension to GameScene so we can become a contactDelegate
    and handle all contact instances.

--- Bapit/GameScene.swift
+++ Bapit/GameScene.swift
@@ -8,42 +8,147 @@
 
 import SpriteKit
 
-let ballRadius: CGFloat = 75
+// source: http://stackoverflow.com/questions/24069703/
+enum ColliderType: UInt32 {
+  case Ball = 1
+  case BottomBorder = 2
+}
+
+struct TapCount: Printable {
+  var hits: Int
+  var misses: Int
+
+  // Allow for easy debugging with description
+  var description: String {
+    return "Hits: \(self.hits) \nMisses: \(self.misses)"
+  }
+}
 
 class GameScene: SKScene {
-  let ball: SKShapeNode = SKShapeNode(circleOfRadius: ballRadius)
+  // MARK: -
+  // MARK: Properties
+  let ballRadius: CGFloat = 50
+
+  var score: TapCount {
+    didSet {
+      scoreLabel.text = "Score: \(self.score.hits)"
+    }
+  }
+
+  let ball: SKShapeNode
+  let scoreLabel: SKLabelNode
+  let bottomBorder: SKNode
+
+  // MARK: -
+  // MARK: Init
+
+  required init(coder: NSCoder) {
+    fatalError("NSCoding not supported")
+  }
+
+  override init(size: CGSize) {
+    self.score = TapCount(hits: 0, misses: 0)
+
+    self.ball = SKShapeNode(circleOfRadius: ballRadius)
+    self.bottomBorder = SKNode()
+    self.scoreLabel = SKLabelNode()
+
+    super.init(size: size)
+  }
 
   override func didMoveToView(view: SKView) {
     self.backgroundColor = SKColor.grayColor()
+
     self.physicsWorld.gravity = CGVectorMake(0.0, -9.8);
+    self.physicsWorld.speed = 1.25
+    self.physicsWorld.contactDelegate = self
+
     self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
 
+    self.addChild(createBall())
+    self.addChild(createBottomBorder())
+    self.addChild(createScoreLabel())
+  }
+
+  // MARK: -
+  // MARK: Elements
+
+  func createBall() -> SKShapeNode {
     ball.fillColor = SKColor.blackColor()
     ball.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+
     ball.physicsBody = SKPhysicsBody(circleOfRadius: ballRadius)
     ball.physicsBody.allowsRotation = false
-    self.addChild(ball)
+    ball.physicsBody.restitution = 0.5
+    ball.physicsBody.categoryBitMask = ColliderType.Ball.toRaw()
+    ball.physicsBody.contactTestBitMask = ColliderType.BottomBorder.toRaw()
+
+    return ball;
   }
-    
+
+  func createBottomBorder() -> SKNode {
+    bottomBorder.physicsBody = SKPhysicsBody(edgeLoopFromRect: CGRectMake(self.frame.origin.x, self.frame.origin.y, self.frame.size.width, 1))
+    bottomBorder.physicsBody.dynamic = false
+    bottomBorder.physicsBody.categoryBitMask = ColliderType.BottomBorder.toRaw()
+
+    return bottomBorder
+  }
+
+  func createScoreLabel() -> SKLabelNode {
+    // Create score label
+    scoreLabel.fontName = "Helvetica"
+    scoreLabel.fontSize = 22
+
+    // Bottom Left
+    scoreLabel.position = CGPointMake(CGRectGetMidX(frame)/3, 20);
+
+    return scoreLabel
+  }
+
+  // MARK: -
+  // MARK: Interaction
+
   override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
     /* Called when a touch begins */
         
     for touch: AnyObject in touches {
       let location = touch.locationInNode(self)
+
       // Only apply an impulse if the touch occured inside the ball
       if ball.containsPoint(location) {
         let point = touch.locationInNode(ball)
+
         // point.x is where inside the ball the touch occured.
         // We're inverting the value so that a touch on the left causes the ball
         // to move to the right, and vice versa.
-        ball.physicsBody.applyImpulse(CGVectorMake(-point.x, 500), atPoint: point)
+
+        // Directly assign the ball velocity so that its current velocity
+        // doesn't negate the added impulse
+        ball.physicsBody.velocity = CGVectorMake(-point.x * 10, 500)
+
+        score.hits++
+      } else {
+        score.misses++
       }
 
-
     }
+
   }
-   
+
   override func update(currentTime: CFTimeInterval) {
     /* Called before each frame is rendered */
+  }
+}
+
+
+extension GameScene: SKPhysicsContactDelegate {
+  func didBeginContact(contact: SKPhysicsContact!) {
+
+    if (contact.bodyA.categoryBitMask == ColliderType.BottomBorder.toRaw() &&
+        contact.bodyB.categoryBitMask == ColliderType.Ball.toRaw()) {
+
+      // Reset count when you hit the bottom
+      score = TapCount(hits: 0, misses: 0)
+    }
   }
 }
